8323db8ac64020ebf888ff6b398d5f447043f1d6
algodesignlp <77603968+algodesignlp@users.noreply.github.com>
2023-01-22 18:18:02 -0500

    UI

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={styles.mainBG}>
         <div  className={styles.description}>
 
 
         </div>
 
 
-        <img src = "capsule.gif"></img>
+        <img src = "dosome.png"></img>
     <br></br>
       
         <h2> Capsule Protocol Landing Soon.</h2>
